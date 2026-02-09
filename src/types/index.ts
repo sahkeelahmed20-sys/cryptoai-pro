7cc0862a675f0f06d2fe4f4be7ff63e8442f5939
sahkeelahmed20-sys <sahkeelahmed20@gmail.com>
2026-02-09 01:05:50 +0100

    Create index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,4 @@
+export * from './trading';
+export type { NewsArticle, SentimentAnalysis } from '../services/news';
+export type { PriceData, OrderBook } from '../services/marketData';
+export type { TradeSignal, BacktestResult, Trade, Strategy } from '../services/trading';
