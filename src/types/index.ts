1a40fd74a21d4907b393662e8b77cdf7bec0888a
sahkeelahmed20-sys <sahkeelahmed20@gmail.com>
2026-02-09 02:20:05 +0100

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,6 @@ export * from './trading';
 export type { NewsArticle, SentimentAnalysis } from '../services/news';
 export type { PriceData, OrderBook } from '../services/marketData';
 export type { TradeSignal, BacktestResult, Trade, Strategy } from '../services/trading';
+export * from './trading';
+export type { PriceData, OrderBook } from '../services/marketData';
+export type { LivePriceData } from '../services/binanceWebSocket';
